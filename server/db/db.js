02e0cda3da45b329e9a2c1b1f63e66218a0bf83e
Frank <mirandofrank@gmail.com>
2023-06-01 09:56:36 -0700

    Made db connection variables dynamic via .env

--- server/db/db.js
+++ server/db/db.js
@@ -2,11 +2,11 @@ require('dotenv').config();
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  host: 'localhost',
+  host: process.env.DB_HOST,
   port: process.env.DB_PORT,
-  user: 'frankmirando',
-  password: '',
-  database: 'products',
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
 });
 
 pool.connect()
